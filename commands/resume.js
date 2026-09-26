646e7db84fd96c3b433b081b0a8c6d0cdac3ff3e
akdtech <93154021+akdtech@users.noreply.github.com>
2026-09-26 14:07:37 +0400

    Restore Discord VC resume

--- commands/resume.js
+++ commands/resume.js
@@ -1,2 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
-module.exports={data:new SlashCommandBuilder().setName("resume").setDescription("Resume Spotify playback."),async execute(i){await i.client.spotify.resume(i.guildId,i.user.id);return i.reply("▶️ Spotify resumed.");}};
+module.exports = {
+  data: new SlashCommandBuilder().setName("resume").setDescription("Resume Discord voice playback."),
+  async execute(i, { music }) {
+    await music.resume(i.guildId);
+    return i.reply("▶️ Music resumed in the Discord voice channel.");
+  }
+};
