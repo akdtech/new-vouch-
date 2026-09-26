226b56c3401d96c3d6a68d78cc44af0ed610b55d
akdtech <93154021+akdtech@users.noreply.github.com>
2026-09-26 13:59:53 +0400

    Convert music control to Spotify Connect

--- commands/skip.js
+++ commands/skip.js
@@ -3,10 +3,9 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("skip")
-    .setDescription("Skip the current track."),
-
-  async execute(interaction, { music }) {
-    await music.skip(interaction.guildId);
-    return interaction.reply("⏭️ Skipped the current track.");
+    .setDescription("Skip to the next track on Spotify."),
+  async execute(interaction) {
+    await interaction.client.spotify.next(interaction.guildId, interaction.user.id);
+    return interaction.reply("⏭️ Skipped to the next Spotify track.");
   }
 };
