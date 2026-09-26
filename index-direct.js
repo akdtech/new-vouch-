9463286a6b00db2cd17fe3b65dda85b0de4730c9
akdtech <93154021+akdtech@users.noreply.github.com>
2026-09-26 16:48:43 +0400

    Enable unlimited related autoplay queue

--- index-direct.js
+++ index-direct.js
@@ -15,6 +15,7 @@ const path = require("path");
 
 const config = require("./config/config");
 const MusicManager = require("./music/DirectMusicManager");
+// Load the unlimited related-autoplay queue after all preload patches so it owns the final autoplay transition.\nrequire("./music/directUnlimitedAutoplayPatch");
 const SpotifyController = require("./spotify/SpotifyController");
 
 console.log("🧹 DEATH Music boot: Discord Voice 24/7 mode.");
